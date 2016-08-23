1a1c5f0f898d19f6318ab86cda599bc4c41d1fa8
Alexander Pánek <a.panek@rewe-group.at>
2016-08-23 14:22:25 +0200

    Added a gulp clean task and renamed gulp coffee -> compile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,19 @@
 var gulp       = require('gulp');
 var gutil      = require('gulp-util');
 var gulpCoffee = require('gulp-coffee');
+var del        = require('del');
 
-gulp.task('coffee', function() {
+
+gulp.task('compile', function() {
   gulp.src('./src/*.coffee')
     .pipe(gulpCoffee({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('./lib/'));
 });
+
+gulp.task('clean', function() {
+  return del.sync([
+    'lib/*'
+  ]);
+});
+
+gulp.task('default', ['clean', 'compile']);
